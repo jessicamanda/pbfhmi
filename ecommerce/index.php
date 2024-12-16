e130230fe142e07aa557c97af0acb30f0c7a5cad
Jessica <jessica.amanda1410@gmail.com>
2024-12-16 08:43:31 +0700

    logout

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
+if(isset($_GET['logout'])){
+  session_unset();
+  session_destroy();
+  header("Location: login-customer.php");
+  exit();
+
+}
 include('../koneksi.php');
 ?>
 <!DOCTYPE html>
