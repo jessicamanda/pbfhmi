4b38048d49804b9668d1dc0b97784124b5982941
Jessica <jessica.amanda1410@gmail.com>
2024-12-19 10:24:24 +0700

    Create register-customer.php

---
+++ ecommerce/register-customer.php
@@ -0,0 +1,90 @@
+<?php session_start(); ?>
+<?php include "../koneksi.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" href="assets/img/logo.jpeg">
+  <title>
+    <?= $appname ?>
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+</head>
+
+<body class="">
+  <main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-100">
+        <div class="container">
+          <center>
+            <div class="card" style="max-width: 360px;">
+              <div class="card-header pb-0 text-start">
+                <p class="mb-0">Welcome to,</p>
+                <h4 class="font-weight-bolder" style="color: #0F5220;"><?= $appname ?> App</h4>
+              </div>
+              <div class="card-body">
+                <form method="post">
+                  <div class="mb-3">
+                    <input type="text" name="nama_lengkap" class="form-control form-control-lg" placeholder="Isi nama lengkap Anda" aria-label="nama_lengkap">
+                  </div>
+                  <div class="mb-3">
+                    <input type="text" name="username" class="form-control form-control-lg" placeholder="Username" aria-label="Username">
+                  </div>
+                  <div class="mb-3">
+                    <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" name="register" class="btn btn-lg btn-lg w-100 mt-4 mb-0 text-white" style="background-color: #0F5220;">Register</button>
+                  </div>
+                  <div class="text-center">
+                    <button class="btn btn-lg btn-lg w-100 mt-2 mb-0 text-white" style="background-color: #0F5220;"><a style="color: white;" href="./login-customer.php">Login</a></button>
+                  </div>
+                </form>
+                <?php
+                if (isset($_POST['register'])) {
+                  $nama_lengkap = htmlspecialchars($_POST['nama_lengkap']);
+                  $username = htmlspecialchars($_POST['username']);
+                  $password = htmlspecialchars($_POST['password']);
+
+                  $con->query("INSERT INTO user (nama_lengkap, username, password) VALUES ('$nama_lengkap', '$username','$password')");
+                  echo "<script>alert('User berhasil disimpan'); document.location.href='./login-customer.php';</script>";
+                }
+                ?>
+              </div>
+            </div>
+          </center>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+</body>
